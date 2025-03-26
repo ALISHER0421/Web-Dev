078a4559af9464db824293d88202e1c7412afb71
Alisher Murat <60564049+ALISHER0421@users.noreply.github.com>
2025-03-26 15:21:11 +0500

    Add files via upload

---
+++ Lab6/Lab6/lab6/src/app/album-detail/album-detail.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {Album} from "../models";
+import {AlbumService} from "../album.service";
+
+@Component({
+  selector: 'app-album-detail',
+  templateUrl: './album-detail.component.html',
+  styleUrls: ['./album-detail.component.css']
+})
+export class AlbumDetailComponent implements OnInit{
+  album: Album;
+  change: boolean = false;
+
+  constructor(private route: ActivatedRoute,
+              private albumService: AlbumService) {
+    this.album = {} as Album;
+  }
+
+  changeable(){
+  this.change = !this.change;
+  }
+
+
+  ngOnInit(): void {
+    // const id = Number(this.route.snapshot.paramMap.get('id'));
+    // if(id){
+    //   let num_id = +id;
+    // }
+    this.route.paramMap.subscribe((params) => {
+      const id = Number(params.get('id'));
+      this.albumService.getAlbum(id).subscribe((album) => {
+        this.album = album;
+      });
+    })
+
+  }
+}
